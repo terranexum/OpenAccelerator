ad820a41b404a12589479426c0857748147c023c
Dahl Winters <dwinters@terranexum.com>
2023-03-15 23:35:16 -0600

    major update - ready for diagram creation

--- README.md
+++ README.md
@@ -1,10 +1,34 @@
 # OpenAccelerator
 
-Improving the adoption of open hardware by making documentation easier to generate, and to allow the relationships between other documents to be automatically visualized using a common diagramming framework from systems engineering.
+This Python code is designed to help increase the adoption of open hardware by making documentation easier to generate, and to allow the relationships between other documents to be automatically visualized using a common diagramming framework from systems engineering.
+
+This code is under development: as of March 15, 2023, text can be extracted from a PDF, topics automatically generated for the logical divisions of content within the PDF, and the divisions organized accordingly into nodes that can be outputted into a diagram. The diagram will likely be generated using Mermaid, which can allow GitHub to render the navigable diagrams.
+
+## Usage Example
+
+Say that you would like to understand how a computer system is built. There are different documents, each written by different people, but all describing one or more topics about computer memory, processors, solid state drives, graphics cards, etc. There is also a summary document describing the different parts of a computer system.
+
+Instead of having a hundred pages of documentation to look through, it would be much more efficient to have a single diagram that provides a navigable interface to explore the rest of the content within the hundred pages of documentation. The top-level diagram would be very much a reflection of the summary document, and within that will be clickable nodes to describe the subsystems (memory, processors, storage, etc). Clicking on any subsystem will take you to a diagram about that subsystem which lists its components, and so on.
+
+## Advantages 
+
+How OpenAccelerator accelerates open hardware documentation upkeep, comprehensibility, and usability:
+
+1. Whenever there is a change to any portion of the hundred pages, or when documentation is added or removed, the diagram can be updated and the connections refreshed.
+2. If you want to change your storage from a magnetic hard drive to a solid state hard drive, a ton of documentation rework will not be necessary. Just replace the magnetic hard drive documentation with solid state hard drive documentation and refresh the diagram.
+3. To extend your computer system for use with other systems, say, monitors, keyboards, etc., you can take the documentation for the diagrams of both systems and generate a super-diagram describing the combination of the two systems.
+
+## Instructions
+
+Any file that is placed in the io/input directory will be used to generate a navigable digaram as output, where the key topics in the different files are extracted and linked together by how related they are. 
+
+Documents can consist of plain text or PDFs; if downloaded from the web, these can even be in HTML format. 
+
+In the future, documentation can be retrieved from webpages and downloaded into io/input so that the diagram can be regenerated upon request. 
 
 ## How to Contribute
 
-To contribute to this repository and help accelerate sustainability action plans (ASAPs) to be created around the world, you can follow these steps:
+To contribute to this repository, you can follow these steps:
 
 1. Fork the repository by clicking on the "Fork" button in the top right corner of this page.
 2. Clone the forked repository to your local machine using Git.
--- src/output_gen/output_gen.py
+++ src/output_gen/output_gen.py
@@ -3,13 +3,35 @@ import os
 import sys
 import textract
 import re
+import nltk
 import urllib.request
+import numpy as np
+import pandas as pd
+import math
+import heapq
+# Library to import pre-trained model for sentence embeddings - installs transformers, torch, torchvision, numpy, scipy, scikit-learn, nltk, huggingface-hubpip
+from sentence_transformers import SentenceTransformer
+# Calculate similarities between sentences
+from sklearn.metrics.pairwise import cosine_similarity
+# Visualization library
+import seaborn as sns
+import matplotlib.pyplot as plt
+# package for finding local minimas
+from scipy.signal import argrelextrema
+
 from collections import defaultdict
 from plantweb.render import render
-from spacy.matcher import Matcher
+from spacy.matcher import PhraseMatcher
 from dotenv import load_dotenv
 
 load_dotenv()
+
+pd.set_option('max_colwidth', 2000)
+pd.options.display.max_rows = 500
+
+# will need downloading only if there's a new installation.
+#nltk.download('punkt')
+#nltk.download('stopwords')
 
 
 '''
@@ -20,7 +42,8 @@ python -m spacy download en_core_web_sm
 pip install plantweb
 pip install python-dotenv
 Tesseract: https://github.com/UB-Mannheim/tesseract/wiki (for Windows)
-
+pip install sentence_transformers
+pip install seaborn
 
 set up .env file with DIR_PATH variable going to the io/input folder in this repo
 
@@ -28,17 +51,92 @@ set up .env file with DIR_PATH variable going to the io/input folder in this rep
 
 # https://towardsdatascience.com/from-text-to-knowledge-the-information-extraction-pipeline-b65e7e30273e
 
-import pandas as pd
-pd.set_option('max_colwidth', 2000)
-pd.options.display.max_rows = 500
+
 
 # Set the directory path to search
 DIR_PATH = os.getenv('DIR_PATH')
 
 nlp = spacy.load('en_core_web_sm')
 
+# Loading a model - 420 MB, will take a while (5 min?) but only on the first run on a new machine so the model files can be downloaded. 
+model = SentenceTransformer('all-mpnet-base-v2')
+
 text = ''
 
+
+
+
+def title_gen(contents, num_sentences):
+    article_text = contents
+    article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+    article_text = re.sub(r'\s+', ' ', article_text)
+    formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )		
+    formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+    sentence_list = nltk.sent_tokenize(article_text)				#divide text into sentences
+    stopwords = nltk.corpus.stopwords.words('english')				
+
+    word_frequencies = {}
+    for word in nltk.word_tokenize(formatted_article_text):			#calculate frequency of each word
+        if word not in stopwords:
+            if word not in word_frequencies.keys():
+                word_frequencies[word] = 1
+            else:
+                word_frequencies[word] += 1
+    maximum_frequncy = max(word_frequencies.values())
+
+    for word in word_frequencies.keys():
+        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)		#weighted frequency
+        
+    sentence_scores = {}
+    for sent in sentence_list:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in word_frequencies.keys():
+                if len(sent.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word]		#calculate each sentence value
+                
+    summary_sentences = heapq.nlargest(num_sentences, sentence_scores, key=sentence_scores.get)		#get top sentences
+
+    summary = ' '.join(summary_sentences)
+    return summary
+
+
+
+def rev_sigmoid(x:float)->float:
+    return (1 / (1 + math.exp(0.5*x)))
+    
+def activate_similarities(similarities:np.array, p_size=10)->np.array:
+        """ Function returns list of weighted sums of activated sentence similarities
+        Args:
+            similarities (numpy array): it should square matrix where each sentence corresponds to another with cosine similarity
+            p_size (int): number of sentences are used to calculate weighted sum 
+        Returns:
+            list: list of weighted sums
+        """
+        # To create weights for sigmoid function we first have to create space. P_size will determine number of sentences used and the size of weights vector.
+        x = np.linspace(-10,10,p_size)
+        # Then we need to apply activation function to the created space
+        y = np.vectorize(rev_sigmoid) 
+        # Because we only apply activation to p_size number of sentences we have to add zeros to neglect the effect of every additional sentence and to match the length ofvector we will multiply
+        activation_weights = np.pad(y(x),(0,similarities.shape[0]-p_size))
+        ### 1. Take each diagonal to the right of the main diagonal
+        diagonals = [similarities.diagonal(each) for each in range(0,similarities.shape[0])]
+        ### 2. Pad each diagonal by zeros at the end. Because each diagonal is different length we should pad it with zeros at the end
+        diagonals = [np.pad(each, (0,similarities.shape[0]-len(each))) for each in diagonals]
+        ### 3. Stack those diagonals into new matrix
+        diagonals = np.stack(diagonals)
+        ### 4. Apply activation weights to each row. Multiply similarities with our activation.
+        diagonals = diagonals * activation_weights.reshape(-1,1)
+        ### 5. Calculate the weighted sum of activated similarities
+        activated_similarities = np.sum(diagonals, axis=0)
+        return activated_similarities
+  
+
+component_map = defaultdict(list)
+component_names = []
+        
 # Loop through all files in the directory
 for root, dirs, files in os.walk(DIR_PATH):
     for file in files:
@@ -52,150 +150,146 @@ for root, dirs, files in os.walk(DIR_PATH):
             # Extract text from the document using textract
             text = textract.process(file_path, language='en')
             
-            # Create a new PDF with reportlab
-            #pdf_path = os.path.splitext(file_path)[0] + '.pdf'
+            # Split text into sentences
+            sentences = text.decode('utf-8').split('. ')
 
-            #if os.path.exists(pdf_path): os.remove(pdf_path) 
+            # Get the length of each sentence
+            sentence_length = [len(each) for each in sentences]
+            # Determine longest outlier
+            long = np.mean(sentence_length) + np.std(sentence_length) *2
+            # Determine shortest outlier
+            short = np.mean(sentence_length) - np.std(sentence_length) *2
+            # Shorten long sentences
+            text = ''
+            for each in sentences:
+                if len(each) > long:
+                    # let's replace all the commas with dots
+                    comma_splitted = each.replace(',', '.')
+                else:
+                    text+= f'{each}. '
+            sentences = text.split('. ')
+            # Now let's concatenate short ones
+            text = ''
+            for each in sentences:
+                if len(each) < short:
+                    text+= f'{each} '
+                else:
+                    text+= f'{each}. '
+
+            # Embed sentences
+            embeddings = model.encode(sentences)
+            print(embeddings.shape)
+
+            # Create similarities matrix
+            similarities = cosine_similarity(embeddings)
+
+            # Apply function: for long sentences, 10 or more sentences are recommended.
+            activated_similarities = activate_similarities(similarities, p_size=5)
+
+            # Find relative minima of the vector. For all local minima and save them to variable with argrelextrema function
+            minima = argrelextrema(activated_similarities, np.less, order=2) # order parameter controls how frequent the splits should be - recommend not changing.
+
+            '''
+            # plotting only
+
+            # Create an empty figure for the plot
+            fig, ax = plt.subplots()
+
+            # plot the flow of the text with activated similarities
+            sns.lineplot(y=activated_similarities, x=range(len(activated_similarities)), ax=ax).set_title('Relative minima');
+
+            # plot vertical lines to see where the split was created
+            plt.vlines(x=minima, ymin=min(activated_similarities), ymax=max(activated_similarities), colors='purple', ls='--', lw=1, label='vline_multiple - full height')
+            '''
+
+            #Get the order number of the sentences which are in splitting points
+            split_points = [each for each in minima[0]]
+
+            # Create empty string
+            text = ''
+
+            for num,each in enumerate(sentences):
+                # Check if sentence is a minimum (splitting point)
+                if num in split_points:
+                    # If it is than add a dot to the end of the sentence and a paragraph before it.
+                    text+=f'\n\n {each}. '
+                else:
+                    # If it is a normal sentence just add a dot to the end and keep adding sentences.
+                    text+=f'{each}. '
+
+            paragraphs = text.split('\n\n')
+            print("num of paragraphs", len(paragraphs))
+
+            titles = []
+            for para in paragraphs:
+                num_sents = len(para.split('. '))
+                titles.append(title_gen(para, 1))
+
+            print("generated titles", titles)
+
+            # Embed paragraphs
+            embeddings = model.encode(paragraphs)
+            #print(embeddings.shape)
+
+            # Create similarities matrix
+            similarities = cosine_similarity(embeddings)
+            #print("num of similarities", len(similarities))
+            #print(similarities)
+
+            connections = []
+            for i in range(len(similarities)):
+                p_start = i
+                row = np.array(similarities[i])
+                idx_one = np.argmax(row)
+                decision_row = np.delete(row, idx_one)
+                #print(decision_row)
+                idx_max = np.argmax(decision_row)
+                #print(idx_max)
+                if idx_one == idx_max:
+                    p_end = idx_max + 1
+                else:
+                    p_end = idx_max
+                #print(p_start, p_end)
+                connections.append([p_start, p_end, titles[p_start], paragraphs[p_start]])
+
+        
+
+            # Generate the PlantUML component diagram
+
+            print('@startuml')
+
+            for i in range(len(connections)):
+                if len(connections[i]) > 0:
+                    node1, node2, title, para = connections[i]
+                    print(f'{node1} --> {node2}')
+
+            print('@enduml')
+
+            '''
+            CONTENT = """
+            actor Foo1
+            boundary Foo2
+            control Foo3
+            entity Foo4
+            database Foo5
+            Foo1 -> Foo2 : To boundary
+            Foo1 -> Foo3 : To control
+            Foo1 -> Foo4 : To entity
+            Foo1 -> Foo5 : To database
+            """
+
+            output = render(
+                    CONTENT,
+                    engine='plantuml',
+                    format='svg',
+                    cacheopts={
+                        'use_cache': False
+                    }
+                )
+            '''
+            print('==> OUTPUT:')
 
 
-# Download the document from the URL
-#url = 'https://www.example.com/document.pdf'
-#urllib.request.urlretrieve(url, 'document.pdf')
+            # or instead of UML, use Mermaid to render the diagram and accompanying paragraph text upon clicking on each object in the diagram.
+            # This will offer a highly condensed and organized view of all documentation sections and how they relate to the overall project being described by the diagram.
 
-# Extract text from the document using textract
-#text = textract.process('City of Denver 2021.pdf')
-
-# Find titles using regular expressions
-titles = re.findall(r'\n\s*[A-Z][A-Za-z0-9\s]+[.:]', text.decode('utf-8'))
-titles = [t.replace("\n", "") for t in titles]
-titles = [t.replace("\r", "") for t in titles]
-titles = [t.replace("\x0c", "") for t in titles]
-
-# Print the titles
-#print(titles)
-
-#title_strs = '|'.join([t for t in titles])
-#print(title_strs)
-
-# Define the pattern for section titles
-#section_title_pattern = [
-#    {"TEXT": {title_strs}}
-#]
-
-#print(section_title_pattern)
-
-matcher = Matcher(nlp.vocab)
-
-# Define a pattern to match sentences containing "title"
-#pattern = [{"LOWER": {"IN": ["title"]}}, {"IS_PUNCT": True, "OP": "?"}, {"POS": "ADJ", "OP": "*"}, {"POS": "NOUN", "OP": "+"}]
-
-#section_title_pattern = [{"label": "SECTION_TITLE", "pattern": pattern} for pattern in patterns]
-
-# Create a pattern for the matcher using the section titles
-#for sent in titles:
-patterns = list([{"TEXT": sent} for sent in titles])
-
-print(patterns)
-#sys.exit(1)
-matcher.add('SENTENCES', patterns)
-
-# Read the text file
-#doc = nlp(open('example.txt', encoding="utf8").read())
-doc = nlp(text)
-matches = matcher(doc)
-
-
-# Print the section titles and their start and end positions
-for match_id, start, end in matches:
-    section_title = doc[start:end].text
-    print(f"Section title: {section_title}, start: {start}, end: {end}")
-
-
-
-''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-'''
-Named Entity Recognition
-'''
-''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-
-
-# Get the entities, labels and explaination to the labels
-table = []
-for ent in doc.ents:
-    table.append([ent.text,ent.label_,spacy.explain(ent.label_)])
-
-# Create a dataframe from the list created above
-df2 = pd.DataFrame(table, columns=['Entity', 'Label','Label_Description']).sort_values(by=['Label'])
-#print(df2)
-
-entities = []
-relations = []
-
-for ent in doc.ents:
-    entities.append((ent.text, ent.label_))
-
-print('entities', entities)
-for token in doc:
-    if token.dep_ in ["nsubj"]: # "amod", "compound"]:
-        relations.append((token.head.text, token.dep_, token.text))
-    elif token.dep_ in ["prep"]: #, "pobj", "dobj"]:
-        prep = token.text
-        for child in token.children:
-            if child.dep_ == "pobj":
-                relations.append((token.head.text, prep, child.text))
-
-component_map = defaultdict(list)
-component_names = set() #entities #set()
-
-# Parse the output from the dependency parsing code
-for line in relations:
-    #line = line.strip()
-    if line:
-        head, rel, tail = line # .split('\t')
-        if rel in ["nsubj"]: # "amod", "compound"]:
-            #print('add to names', line)
-            component_names.add(head)
-        else: # rel in ["prep", "pobj", "dobj"]: #rel == 'prep': # and tail.startswith('n'):
-            #print('add to map', line)
-            component_map[head].append(tail)
-
-# Generate the PlantUML component diagram
-
-print('@startuml')
-
-for component in component_names:
-
-    if len(component_map[component]) > 0:
-        #print(f'component {component} {{')
-        #print(f'{component_map[component]}')
-        #print('}')
-        for connection in component_map[component]:
-            print(f'{component} --> {connection}')
-
-print('@enduml')
-
-
-'''
-CONTENT = """
-actor Foo1
-boundary Foo2
-control Foo3
-entity Foo4
-database Foo5
-Foo1 -> Foo2 : To boundary
-Foo1 -> Foo3 : To control
-Foo1 -> Foo4 : To entity
-Foo1 -> Foo5 : To database
-"""
-
-output = render(
-        CONTENT,
-        engine='plantuml',
-        format='svg',
-        cacheopts={
-            'use_cache': False
-        }
-    )
-'''
-print('==> OUTPUT:')
-#print(output)
